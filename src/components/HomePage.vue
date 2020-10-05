19e604046cc0ea084227b05b9f1e317683461982
Sanjaya Satyal <Sanjaya-Satyal@ip-192-168-86-187.eu-west-1.compute.internal>
2020-10-05 17:14:18 -0400

    render entries in year route

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -2,12 +2,20 @@
   <div class="hello">
     <h1>March Sadness</h1>
     <h2>what famous people in sports look like</h2>
-    <!-- <router-link to="/foo">Go to Foo</router-link> -->
+    <router-link to="/foo">Go to Foo</router-link>
+    <div v-for="collection in data" :key="collection.year">
+      <router-link
+        :to="`/${collection.year}`"
+        :entries="collection.entries"
+        :year="collection.year"
+      >
+        {{ collection.year }} Entries
+      </router-link>
+    </div>
     <div v-for="(item, index) in names" :key="index">
       <p>{{item.name}}</p>
       <p>{{item.looks_like}}</p>
       <img :src='`${item.photo_url}`'/>
-      <img>
     </div>
   </div>
 </template>
@@ -16,13 +24,17 @@
 import data from '../data.json'
 
 export default {
-  name: 'HelloWorld',
+  name: 'HomePage',
   props: {
     msg: String
   },
   computed: {
     names() {
       return data[0].entries.map(item => item);
+    },
+    data() {
+      console.log({data})
+      return data;
     }
   }
 }
---
+++ src/components/Year.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>hello {{foo}}</div>
+  <div v-for="item in yearEntries" :key="item.photo_url">
+    <p>{{item.name}}</p>
+    <p>{{item.looks_like}}</p>
+    <img :src='`${item.photo_url}`'/>
+  </div>
+</template>
+
+<script>
+import data from '../data.json'
+export default {
+  name: 'Year',
+  props: {
+    entries: Array,
+    year: String,
+    foo: String,
+  },
+  computed: {
+    yearEntries() {
+      return data.filter(item => item.year === this.$route.params.year)[0].entries
+    }
+  },
+}
+</script>
